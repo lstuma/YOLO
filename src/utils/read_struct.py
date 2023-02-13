Dev(fixed bugs):  - fixed calculation of IoU Dev(refactoring):  - renamed function get_int() to _get_value()

src/utils/read_struct.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 def read_struct(file):
 
-    def get_int(s):
+    def _get_value(s):
         s = s.split("=")[1]
         return int(s)
 
@@ -16,11 +16,11 @@ def read_struct(file):
     for i in lst:
         match i[0]:
             case "CONV":
-                struct.append(nn.Conv2d(get_int(i[1]), get_int(i[2]), get_int(i[3]), stride=get_int(i[4])))
+                struct.append(nn.Conv2d(_get_value(i[1]), _get_value(i[2]), _get_value(i[3]), stride=_get_value(i[4])))
             case "MAXP":
-                struct.append(nn.MaxPool2d(get_int(i[1]), stride=get_int(i[2]), padding=get_int(i[3])))
+                struct.append(nn.MaxPool2d(_get_value(i[1]), stride=_get_value(i[2]), padding=_get_value(i[3])))
             case "LINL":
-                struct.append(nn.Linear(get_int(i[1]), get_int(i[2])))
+                struct.append(nn.Linear(_get_value(i[1]), _get_value(i[2])))
             case other:
                 continue
         struct.append(nn.ReLU())